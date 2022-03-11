cd46cf06727b57dfb9200545b7b3c00323b4e4c2
jworkss <flytoshine@gmail.com>
2022-03-11 05:33:09 -0300

    servicio y servidor de tareas listo

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from "@angular/common/http"
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-task';
@@ -7,11 +8,14 @@ import { TASKS } from 'src/app/mock-task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = ' http://localhost:5001/tasks'
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
   getTask(): Observable <Task[]>{
-    const tasks = of(TASKS);
-    return tasks
+
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
